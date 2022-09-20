d89d821750e94aed0603bd50841a329888be68ae
markemoonewriting <markedwardmoone@gmail.com>
2022-09-19 20:00:51 -0400

    Diary of the guy who doesn't know how to use classes property

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,262 +1,267 @@
-﻿namespace CaffeineOverdoseSimulator
+﻿using System;
+using Actions;
+
+namespace CaffeineOverdoseSimulator;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("CAFFEINE OVERDOSE SIMULATOR");
-            Console.WriteLine("Press any key to begin...");
-            Console.ReadKey();
-            int time = 0;
-            int tiredness;
-            int anxiety;
-            int focus;
-            int caffeine = 0;
-            bool atHome = true;
-            bool driving = false;
-            bool atWork = false;
-            bool showered = false;
-            bool ateBreakfast = false;
-            bool wentToLunch = false;
-            tiredness = StartTiredLevel();
-            anxiety = StartAnxietyLevel();
-            focus = StartFocusLevel();
-            Console.WriteLine("Your fitful night of poor sleep must come to an end. Your front door slams in the distance...");
-            Console.WriteLine($"That was your roommate leaving for work, meaning it's {ParseTime(time)}, time for you to get your day started.");
-            Console.WriteLine("You need to head out the door by 7:40am to make it to work on time at 8:00am.");
-            Console.WriteLine(" ");
-            Console.WriteLine("Here are the facts:");
-            Console.WriteLine(ParseTiredness(tiredness));
-            Console.WriteLine(ParseAnxiety(anxiety));
-            Console.WriteLine(ParseFocus(focus));
+        Console.WriteLine("CAFFEINE OVERDOSE SIMULATOR");
+        Console.WriteLine("Press any key to begin...");
+        Console.ReadKey();
+        int time = 0;
+        int tiredness;
+        int anxiety;
+        int focus;
+        int caffeine = 0;
+        bool atHome = true;
+        bool driving = false;
+        bool atWork = false;
+        bool showered = false;
+        bool ateBreakfast = false;
+        bool wentToLunch = false;
+        tiredness = StartTiredLevel();
+        anxiety = StartAnxietyLevel();
+        focus = StartFocusLevel();
+        Console.WriteLine("Your fitful night of poor sleep must come to an end. Your front door slams in the distance...");
+        Console.WriteLine($"That was your roommate leaving for work, meaning it's {ParseTime(time)}, time for you to get your day started.");
+        Console.WriteLine("Your alarm goes off in ten minutes.");
+        Console.WriteLine("You need to head out the door by 7:40am to make it to work on time at 8:00am.");
+        Console.WriteLine(" ");
+        Console.WriteLine("Here are the facts:");
+        Console.WriteLine(ParseTiredness(tiredness));
+        Console.WriteLine(ParseAnxiety(anxiety));
+        Console.WriteLine(ParseFocus(focus));
 
-            
-            
-           
-        }
+    }
 
-        static public int StartTiredLevel()
-        {
-            Random rnd = new();
-            return rnd.Next(40, 58);
-            
-        }
-        static public int StartAnxietyLevel()
-        {
-            Random rnd = new();
-            return rnd.Next(20, 38);
-        }
-        static public int StartFocusLevel()
-        {
-            Random rnd = new();
-            return rnd.Next(30, 49);
-        }
+    static int StartTiredLevel()
+    {
+        Random rnd = new();
+        return rnd.Next(39, 58);
 
-        static public string ParseTiredness(int level)
-        {
-            if (level <= 9)
-            {
-                return "You feel fully awake!";
-            }
-            else if (level <= 19)
-            {
-                return "You don't hardly feel tired at all.";
-            }
-            else if (level <= 29)
-            {
-                return "You feel on the whole very awake.";
-            }
-            else if (level <= 39)
-            {
-                return "You feel no more tired than you virtually always do.";
-            }
-            else if (level <= 49)
-            {
-                return "Your thoughts are with sleep, but there's no sense that it has to come soon.";
-            }
-            else if (level <= 59)
-            {
-                return "You find yourself yawning uncontrollably from every now and then.";
-            }
-            else if (level <= 69)
-            {
-                return "Your eyes water from time to time.";
-            }
-            else if (level <= 79)
-            {
-                return "You catch yourself closing your eyes involuntarily at inopportune moments.";
-            }
-            else if (level <= 89)
-            {
-                return "Your head seems to be magnetically attracted to whatever is in front of you.";
-            }
-            else if (level <= 99)
-            {
-                return "The line separating the real world and dream land is beginning to blur.";
-            }
-            else
-            {
-                return "You've passed out!";
-            } 
-                
-        }
-        static public string ParseTime(int time)
-        {
-            string ampm = "am";
-            int hour = (time / 6) + 7;
-            int minute = (time % 6) * 10;
-            if (hour == 12)
-            {
-                ampm = "pm";
-            }
-            if (hour >= 13)
-            {
-                hour = hour - 12;
-            }
-            return hour + ":" + minute + "0" + ampm;
-                 
-        }
-        static public string ParseAnxiety(int level)
-        {
-            if (level <= 9)
-            {
-                return "You haven't felt serenity like this since the summer after graduation!";
-            }
-            else if (level <= 19)
-            {
-                return "You feel pretty calm.";
-            }
-            else if (level <= 29)
-            {
-                return "You dread the tasks ahead of you, but aren't really bothered by them.";
-            }
-            else if (level <= 39)
-            {
-                return "You feel jittery, but you doubt anyone else could tell.";
-            }
-            else if (level <= 49)
-            {
-                return "You can't seem to stop yourself from bouncing your leg up and down.";
-            }
-            else if (level <= 59)
-            {
-                return "Your occasionally feel your heart pick up speed for no apparently reason.";
-            }
-            else if (level <= 69)
-            {
-                return "The pool of sweat underneath your shirt is unwarranted by cubicle work.";
-            }
-            else if (level <= 79)
-            {
-                return "Your hands tremble to the point where you've lost some use of them.";
-            }
-            else if (level <= 89)
-            {
-                return "Any sound other than the drone of the office HVAC system makes you jump in your seat.";
-            }
-            else if (level <= 99)
-            {
-                return "Your mouth is dry. Your fingertips are numb. Your can't control your breathing.";
-            }
-            else
-            {
-                return "Oh no, you're having one of your indomitable panic attacks!";
-            }
-        }
-        static public string ParseFocus(int level)
-        {
-            if (level <= 9)
-            {
-                return "You cannot resist the compulsion to look at anything other than work!";
-            }
-            else if (level <= 19)
-            {
-                return "You can't stay focused on anything for more than few seconds.";
-            }
-            else if (level <= 29)
-            {
-                return "Your boredom has become a powerful distraction unto itself.";
-            }
-            else if (level <= 39)
-            {
-                return "You're capable of completing sequences of actions, but you can't remember doing them after you've finished.";
-            }
-            else if (level <= 49)
-            {
-                return "Your thinking feels slowed, but you're capable of deliberation.";
-            }
-            else if (level <= 59)
-            {
-                return "With some effort, you're able to stay on task.";
-            }
-            else if (level <= 69)
-            {
-                return "You feel fully capable of any task you might need to accomplish.";
-            }
-            else if (level <= 79)
-            {
-                return "Your attention to detail is very minute and accurate.";
-            }
-            else if (level <= 89)
-            {
-                return "You're reaching a flow state. Even the most mundane task gives you pleasure.";
-            }
-            else if (level <= 99)
-            {
-                return "You're handling information in the world as if you were breathing it.";
-            }
-            else
-            {
-                return "You have perfect focus!";
-            }
-        }
-        static public void PassTenMinutes(int time, int tiredness, int focus, int anxiety, int caffeine)
-        {
-            time = time++;
-            if (time % 6 == 0)
-            {
-                tiredness = tiredness + 3;
-                Random rnd = new();
-                int focusCoinFlip = rnd.Next(1, 2);
-                if (focusCoinFlip % 2 == 0)
-                {
-                    focus = focus + 10;
-                } else
-                {
-                    focus = focus - 10;
-                }
-                anxiety = (int)(anxiety * Math.Pow(0.5, 0.17));
-                caffeine = (int)(caffeine * Math.Pow(0.5, 0.042));
+    }
+    static int StartAnxietyLevel()
+    {
+        Random rnd = new();
+        return rnd.Next(19, 38);
+    }
+    static int StartFocusLevel()
+    {
+        Random rnd = new();
+        return rnd.Next(29, 48);
+    }
 
-            }
-        }
-        static public int CalculateNewAnxiety(int caffeine, int tiredness, int anxiety)
+    static string ParseTiredness(int tiredness)
+    {
+        if (tiredness <= 9)
         {
-            int addedAnxiety = (caffeine / 50) - (tiredness / 100);
-            return anxiety + addedAnxiety;
+            return "You feel fully awake!";
         }
-
-        static public int DirectlyChangeAnxiety(int directAnxiety, int anxiety)
+        else if (tiredness <= 19)
         {
-            return anxiety + directAnxiety;
+            return "You don't hardly feel tired at all.";
         }
-
-        static public int DirectlyChangeTiredness(int directTiredness, int tiredness)
+        else if (tiredness <= 29)
         {
-            return tiredness + directTiredness;
+            return "You feel on the whole very awake.";
         }
-
-        static public int DirectlyChangeFocus(int directFocus, int focus)
+        else if (tiredness <= 39)
         {
-            return focus + directFocus;
+            return "You feel no more tired than you virtually always do.";
         }
-
-        static public int DirectAddCaffeine(int caffeine, int dose)
+        else if (tiredness <= 49)
         {
-            return caffeine + dose;
+            return "Your thoughts are with sleep, but there's no sense that it has to come soon.";
+        }
+        else if (tiredness <= 59)
+        {
+            return "You find yourself yawning uncontrollably from every now and then.";
+        }
+        else if (tiredness <= 69)
+        {
+            return "Your eyes water from time to time.";
+        }
+        else if (tiredness <= 79)
+        {
+            return "You catch yourself closing your eyes involuntarily at inopportune moments.";
+        }
+        else if (tiredness <= 89)
+        {
+            return "Your head seems to be magnetically attracted to whatever is in front of you.";
+        }
+        else if (tiredness <= 99)
+        {
+            return "The line separating the real world and dream land is beginning to blur.";
+        }
+        else
+        {
+            return "You've passed out!";
         }
 
     }
+    static string ParseTime(int time)
+    {
+        string ampm = "am";
+        int hour = (time / 6) + 7;
+        int minute = (time % 6) * 10;
+        if (hour == 12)
+        {
+            ampm = "pm";
+        }
+        if (hour >= 13)
+        {
+            hour = hour - 12;
+        }
+        return hour + ":" + minute + "0" + ampm;
+
+    }
+    static string ParseAnxiety(int anxiety)
+    {
+        if (anxiety <= 9)
+        {
+            return "You haven't felt serenity like this since the summer after graduation!";
+        }
+        else if (anxiety <= 19)
+        {
+            return "You feel pretty calm.";
+        }
+        else if (anxiety <= 29)
+        {
+            return "You dread the tasks ahead of you, but aren't really bothered by them.";
+        }
+        else if (anxiety <= 39)
+        {
+            return "You feel jittery, but you doubt anyone else could tell.";
+        }
+        else if (anxiety <= 49)
+        {
+            return "You can't seem to stop yourself from bouncing your leg up and down.";
+        }
+        else if (anxiety <= 59)
+        {
+            return "Your occasionally feel your heart pick up speed for no apparently reason.";
+        }
+        else if (anxiety <= 69)
+        {
+            return "The pool of sweat underneath your shirt is unwarranted by cubicle work.";
+        }
+        else if (anxiety <= 79)
+        {
+            return "Your hands tremble to the point where you've lost some use of them.";
+        }
+        else if (anxiety <= 89)
+        {
+            return "Any sound other than the drone of the office HVAC system makes you jump in your seat.";
+        }
+        else if (anxiety <= 99)
+        {
+            return "Your mouth is dry. Your fingertips are numb. Your can't control your breathing.";
+        }
+        else
+        {
+            return "Oh no, you're having one of your indomitable panic attacks!";
+        }
+    }
+    static string ParseFocus(int focus)
+    {
+        if (focus <= 9)
+        {
+            return "You cannot resist the compulsion to look at anything other than work!";
+        }
+        else if (focus <= 19)
+        {
+            return "You can't stay focused on anything for more than few seconds.";
+        }
+        else if (focus <= 29)
+        {
+            return "Your boredom has become a powerful distraction unto itself.";
+        }
+        else if (focus <= 39)
+        {
+            return "You're capable of completing sequences of actions, but you can't remember doing them after you've finished.";
+        }
+        else if (focus <= 49)
+        {
+            return "Your thinking feels slowed, but you're capable of deliberation.";
+        }
+        else if (focus <= 59)
+        {
+            return "With some effort, you're able to stay on task.";
+        }
+        else if (focus <= 69)
+        {
+            return "You feel fully capable of any task you might need to accomplish.";
+        }
+        else if (focus <= 79)
+        {
+            return "Your attention to detail is very minute and accurate.";
+        }
+        else if (focus <= 89)
+        {
+            return "You're reaching a flow state. Even the most mundane task gives you pleasure.";
+        }
+        else if (focus <= 99)
+        {
+            return "You're handling information in the world as if you were breathing it.";
+        }
+        else
+        {
+            return "You have perfect focus!";
+        }
+    }
+    static void PassTenMinutes(int time, int tiredness, int focus, int anxiety, int caffeine)
+    {
+        time = time++;
+        if (time % 6 == 0)
+        {
+            tiredness = tiredness + 3;
+            Random rnd = new();
+            int focusCoinFlip = rnd.Next(1, 2);
+            if (focusCoinFlip % 2 == 0)
+            {
+                focus = focus + 10;
+            }
+            else
+            {
+                focus = focus - 10;
+            }
+            anxiety = (int)(anxiety * Math.Pow(0.5, 0.17));
+            caffeine = (int)(caffeine * Math.Pow(0.5, 0.042));
+
+        }
+    }
+    static int CalculateNewAnxiety(int caffeine, int tiredness, int anxiety)
+    {
+        int addedAnxiety = (caffeine / 50) - (tiredness / 100);
+        return anxiety + addedAnxiety;
+    }
+
+    static int DirectlyChangeAnxiety(int directAnxiety, int anxiety)
+    {
+        return anxiety + directAnxiety;
+    }
+
+    static int DirectlyChangeTiredness(int directTiredness, int tiredness)
+    {
+        return tiredness + directTiredness;
+    }
+
+    static int DirectlyChangeFocus(int directFocus, int focus)
+    {
+        return focus + directFocus;
+    }
+
+    static int DirectAddCaffeine(int caffeine, int dose)
+    {
+        return caffeine + dose;
+    }
+
+    
+
+
+
 }
 
 
